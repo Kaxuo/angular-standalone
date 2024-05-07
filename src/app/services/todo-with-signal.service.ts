bef4026511cdd441a79dc98fc397812952fd4a08
Kaxuo <Kaxuo@hotmail.com>
2024-05-07 18:07:27 +0200

    added another way to do things

--- src/app/services/todo-with-signal.service.ts
+++ src/app/services/todo-with-signal.service.ts
@@ -36,3 +36,31 @@ export class TodosWithSignalService {
     this.todos.mutate(todos => (todos[index] = post));
   };
 }
+
+
+// AddProduct(_product: SalesProduct) {
+//   this.productlist.mutate(previous => previous.push(_product))
+// }
+
+// UpdateProduct(_product: SalesProduct) {
+//   let newarry = this.productlist().map(item => {
+//     return item.slno === _product.slno ? _product : item
+//   });
+//   this.productlist.set(newarry);
+// }
+
+// RemoveProduct(slno: number) {
+//   this.productlist.update(previous => previous.filter(item => item.slno !== slno));
+// }
+
+// GetProductbyCode(slno: number) {
+//   this.productitem.set(this.productlist().find(item => item.slno === slno) as SalesProduct);
+// }
+
+// totalqty = computed(() => this.productlist().length);
+// summarytotal = computed(() => this.productlist().reduce((prev: any, curr: SalesProduct) => {
+//   return prev + curr.total
+// }, 0))
+
+// summarytax = computed(() => (this.summarytotal() * 7) / 100);
+// summarynettotal = computed(() => (this.summarytotal() + this.summarytax()));
