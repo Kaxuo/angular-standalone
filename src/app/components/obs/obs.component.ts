582f033064e8c7a31ad03ae4c771f3ea721bb4ac
Nguyen Hoang <Hoang.Nguyen@nsi-sa.be>
2024-06-19 11:52:43 +0200

    obs done

--- src/app/components/obs/obs.component.ts
+++ src/app/components/obs/obs.component.ts
@@ -1,6 +1,6 @@
 import {CommonModule} from '@angular/common';
 import {Component} from '@angular/core';
-import {Observable, concatMap, delay, finalize, from, mergeMap, of, switchMap, tap} from 'rxjs';
+import {Observable, concatMap, delay, from, mergeMap, of, switchMap, tap} from 'rxjs';
 
 @Component({
   selector: 'app-obs',
@@ -10,30 +10,30 @@ import {Observable, concatMap, delay, finalize, from, mergeMap, of, switchMap, t
   styleUrl: './obs.component.scss',
 })
 export class ObsComponent {
-  data: Observable<number> = from([1, 2, 3, 4, 5])
-    .pipe(delay(500))
-    .subscribe(x => console.log(x));
+  // https://dev.to/kinginit/rxjs-mergemap-vs-switchmap-vs-concatmap-vs-exhaustmap-5gpg
+
+  data: Observable<number> = from([1, 2, 3, 4, 5]);
   mergeMapObs$: Observable<number>;
   concatMapObs$: Observable<number>;
   switchMapObs$: Observable<number>;
 
   ngOnInit() {
-    // this.mergeMapObs$ = of(this.data).pipe(
-    //   mergeMap(num => num),
-    //   tap(v => console.log(v)),
-    //   finalize(() => console.log('mergeMap Done')),
-    // );
-    // this.concatMapObs$ = of(this.data).pipe(
-    //   concatMap(num => num),
-    //   tap(v => console.log(v)),
-    //   finalize(() => console.log('ConcatMap Done')),
-    // );
-    // this.switchMapObs$ = of(this.data).pipe(
-    //   switchMap(num => num),
-    //   tap(v => console.log(v)),
-    //   finalize(() => console.log('Switchmap Done')),
-    // );
+    // mergeMap: Unlike concatMap, mergeMap does not wait for the previous call to finish. It emits all requests as they come in, processing them at the same time. However, the response order may not match the request order.
+
+    this.mergeMapObs$ = this.operator(mergeMap);
+
+    // concatMap: This operator creates a queue of requests and handles them one after the other. It waits for the previous call to finish before making the next one. It maintains the order of requests and creates a link between them.
+    this.concatMapObs$ = this.operator(concatMap);
+
+    // switchMap: When a bunch of values are emitted quickly, switchMap only cares about the latest one and ignores the rest. It does not wait for the previous calls to complete before making new ones.
+    this.switchMapObs$ = this.operator(switchMap);
   }
 
-  mergeMapIncrement() {}
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  operator(operator: any): Observable<number> {
+    return this.data.pipe(
+      operator((item: number) => of(item).pipe(delay(1000))),
+      tap(x => console.log(x)),
+    );
+  }
 }
