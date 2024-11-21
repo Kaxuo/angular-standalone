f1536c1e35ce5cd2d22ebf85cba7a8858ee52372
Hoang Nguyen <hoang.nguyen@ageas.com>
2024-11-21 17:02:52 +0100

    progress excel

--- src/app/components/rules-guideline-ageas/rules-guideline-ageas.component.ts
+++ src/app/components/rules-guideline-ageas/rules-guideline-ageas.component.ts
@@ -16,7 +16,13 @@ export class RulesGuidelineAgeasComponent {
   private gridApi!: GridApi<any>;
   themeClass = 'ag-theme-quartz';
 
-  colDefs: ColDef[] = [{field: 'rules'}, {field: 'id'}, {field: 'comment'}];
+  colDefs: ColDef[] = [
+    {field: 'system', cellStyle: {'white-space': 'pre-wrap'}},
+    {field: 'rules'},
+    {field: 'id'},
+    // {field: 'table_column', cellStyle: {'white-space': 'pre-wrap'}},
+    {field: 'comment', cellStyle: {'white-space': 'pre-wrap'}},
+  ];
   defaultColDef: ColDef = {
     flex: 1,
     wrapText: true,
@@ -24,19 +30,76 @@ export class RulesGuidelineAgeasComponent {
   };
   rowData = [
     new RuleDB(
-      'SQL - Data type declaration',
-      'General Rule Y',
+      'SQL',
+      'Data type declaration',
+      'General Rule',
+      '',
+      'SQL assigns a certain type to columns , prevent other types to be input.\nHowever, should we add it to .NET too so that the user can see their error since SQL only returns generic errors',
+    ),
+    new RuleDB(
+      'SQL',
+      'Value required',
+      'General Rule',
+      '',
+      'Value of columns can be mandatory based on how SQL is configured',
+    ),
+    new RuleDB(
+      'SQL',
+      'All values of %columnName% should be unique',
+      '234,20 / 244,00',
+      'RRC-Conv:RC_CONV // \nOther BS Items:OBSI_ITEM //',
       'SQL assigns a certain type to columns , prevent other types to be input',
     ),
     new RuleDB(
-      '.NET - IF %columnName% == %variable% then Sum of [Columns] to be between 0 and 100 % (included)',
+      'SQL\n.NET',
+      'Value of %columnName% should not be empty , nor contain comma or semicolon',
+      '252,00 / 252,10 / 253,00',
+      'Asset Pool conv:AP_AP//\nAsset Pool Ul:AP_AP//\nOther BS:OBSI_ITEM  //',
+      'Should do both for the users to see the errors',
+    ),
+    new RuleDB(
+      '.NET',
+      'IF %columnName% == %variable% then Sum of [Columns] to be between 0 and 100 % (included)',
       '16,2',
+      'Equity indices:IDX_(1...5)_FSIR',
       'Should 0 and 100 % be dynamic ?',
     ),
-    new RuleDB('.NET - Value of %columnName% to be between 0 and 1 (included)', '19,2', 'Should 0 and 1 be dynamic ?'),
-    new RuleDB('.NET - Value of %columnName% should be a value of [RangesOfValue]', '142,00 ; 156,00', ''),
-    new RuleDB('Unknown Rules', '205,10', 'Need more info'),
-    new RuleDB('Formulas ?', '218,00 ; 218,10 ; 218,20 ; 222,00', 'Need more info'),
+    new RuleDB(
+      '.NET',
+      'Value of %columnName% to be between 0 and 1 (included)',
+      '19,2',
+      'Asset Pool UL:AP_DCRE //',
+      'Should 0 and 1 be dynamic ?',
+    ),
+    new RuleDB(
+      '.NET',
+      'Value of %columnName% should be a value of [RangesOfValue]',
+      '142,00 / 156,00 / 222,20 / 232,00 / 234,10 / 234,20 / 273,10 / 282,01 -> 282,18 / 285,00 / 290,00 -> 292,00 / 304,00 ',
+      'Other BS:OBSI_CUR /\nOBSI_LIFE_FSIR //\nGeneral:G_TR_FSIR //\nMP_UL:MP_RRC_Basis // \nRRC_Conv:RC_CONVENTION /RC_SWITCH_OFF_FLOOR //\nMP_CONV:MP_SII_LOB /\nMP_RiskDrive_XXXX //\nOther BS:OBSI_ITEM OBSI_INTG OBSI_TYPE_BS//\nCPDExposureType1:CP1_RISK_XXX',
+      'If there are only two values ( specially 0 or 1 ) should it be a different rule? ',
+    ),
+    new RuleDB(
+      '.NET',
+      'Value of %columnName1% should be a value of %value1% AND value of %columnName2% should be different than %value2% then %columnName3% cannot be null but can be 0',
+      '230,00',
+      'MP Conv:MP_GPS',
+      '',
+    ),
+    new RuleDB(
+      'SQL\n.NET',
+      'Value of %columnName% should be equal or higher than %variable%',
+      '284,00 / 295,00 / 305,00 / 315,00 ',
+      'General:G_TNDTL //\nCPDexposuresType1 :CP1_COLLAT_MRA //\n AP_Conv:AP_CAPI // \nCounterPartiesType2:CP2_AP',
+      'Should create the constraint in sql and also code in .NET, easy to do',
+    ),
+    new RuleDB(
+      '???',
+      "I am an idiot, I don't understand",
+      '205,10 / 222,10 / 306,00 / 309,00 / 316,00 / 317,00',
+      '',
+      'Need more info',
+    ),
+    new RuleDB('???', 'Formulas ?', '218,00 / 218,10 / 218,20 / 222,00', '', 'Need more info'),
   ];
 
   ngOnInit() {
@@ -63,12 +126,18 @@ export class RulesGuidelineAgeasComponent {
 }
 
 export class RuleDB {
+  system: string;
   rules: string;
   id: string;
+  table_column: string;
   comment: string;
-  constructor(rules: string, id: string, comment: string) {
+  constructor(system: string, rules: string, id: string, table_column: string, comment: string) {
+    this.system = system;
     this.rules = rules;
     this.id = id;
+    this.table_column = table_column;
     this.comment = comment;
   }
 }
+
+export const twovalues = [''];
